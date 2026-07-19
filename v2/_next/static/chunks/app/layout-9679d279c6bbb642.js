acc24816444ccede7e8b05cc474a1c2e459dd0bc
Сергей <sergej@MacBook-Pro-3.local>
2026-07-19 09:20:04 +0300

    deploy: update alina v2 revision

---
+++ v2/_next/static/chunks/app/layout-9679d279c6bbb642.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2272:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.t.bind(n,2895,23))},2895:()=>{},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[533,94,441,684,358],()=>s(2272)),_N_E=e.O()}]);
--- v2/_next/static/chunks/app/layout-a6ba3cf7bee55b62.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2895:()=>{},9324:()=>{},9576:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.t.bind(n,2895,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[533,94,441,684,358],()=>s(9576)),_N_E=e.O()}]);
